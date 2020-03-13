875282f098ecd6857f2cf6b4de1ecc83cc10e3eb
jakebooker56 <45285591+jakebooker56@users.noreply.github.com>
2020-03-13 10:39:57 +0000

    negative tests

--- test/v1/models/requestData/CreateCisDeductionsRequestModelSpec.scala
+++ test/v1/models/requestData/CreateCisDeductionsRequestModelSpec.scala
@@ -48,18 +48,54 @@ class CreateCisDeductionsRequestModelSpec extends UnitSpec {
       |""".stripMargin
   }
 
+  val testNegativeJson: JsValue = Json.parse {
+    """
+      |{
+      |  "fromDate": "2019-04-06" ,
+      |  "toDate": "2020-04-05",
+      |  "contractorName": "Bovis",
+      |  "employerRef": "BV40092",
+      |  "periodData": [
+      |      {
+      |      "deductionAmount": -355.00,
+      |      "deductionFromDate": "2019-06-06",
+      |      "deductionToDate": "2019-07-05",
+      |      "costOfMaterials": -35.00,
+      |      "grossAmountPaid": -1457.00
+      |    },
+      |    {
+      |      "deductionAmount": -355.00,
+      |      "deductionFromDate": "2019-07-06",
+      |      "deductionToDate": "2019-08-05",
+      |      "costOfMaterials": -35.00,
+      |      "grossAmountPaid": -1457.00
+      |    }
+      |  ]
+      |}
+      |""".stripMargin
+  }
+
   val cisDeductionsRequestObj: CreateCisDeductionsRequestModel = CreateCisDeductionsRequestModel("2019-04-06", "2020-04-05", "Bovis", "BV40092",
     Seq(
       PeriodData(355.00, "2019-06-06", "2019-07-05", Some(35.00), 1457.00),
       PeriodData(355.00, "2019-07-06", "2019-08-05", Some(35.00), 1457.00)
     )
   )
-
+  val testNegative: CreateCisDeductionsRequestModel = CreateCisDeductionsRequestModel("2019-04-06", "2020-04-05", "Bovis", "BV40092",
+    Seq(
+      PeriodData(-355.00, "2019-06-06", "2019-07-05", Some(-35.00), -1457.00),
+      PeriodData(-355.00, "2019-07-06", "2019-08-05", Some(-35.00), -1457.00)
+    )
+  )
   "CisDeductionsRequestModel" when {
     " read from valid JSON " should {
       "return the expected CisDeductionsRequestBody" in {
         Json.toJson(cisDeductionsRequestObj) shouldBe cisDeductionsRequestJson
       }
+      "return the expected negative test for CisDeductionsRequestBody" in {
+        Json.toJson(testNegative) shouldBe testNegativeJson
+      }
     }
   }
+
 }
