0afbccbbdf391873c8a24298988cdb4cc650f059
jakebooker56 <45285591+jakebooker56@users.noreply.github.com>
2020-04-20 17:43:59 +0100

    Amend model and tests

---
+++ app/v1/models/request/AmendRequestModel.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.request
+
+import play.api.libs.json.{Json, Reads, Writes}
+
+  case class AmendRequestModel(
+                                 fromDate: String,
+                                 toDate: String,
+                                 contractorName: String,
+                                 employerRef: String,
+                                 periodData: Seq[PeriodDetails]
+                               )
+
+  object AmendRequestModel {
+    implicit val reads: Reads[AmendRequestModel] = Json.reads[AmendRequestModel]
+    implicit val writes: Writes[AmendRequestModel] = Json.writes[AmendRequestModel]
+  }
+
