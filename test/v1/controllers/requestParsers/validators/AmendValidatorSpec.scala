d267cf68eacef39e5132b7d617dfe8f6311761d8
zachhall1234 <29452017+zachhall1234@users.noreply.github.com>
2020-04-23 10:13:09 +0100

    MTDSA-5253 unused imports

--- test/v1/controllers/requestParsers/validators/AmendValidatorSpec.scala
+++ test/v1/controllers/requestParsers/validators/AmendValidatorSpec.scala
@@ -19,7 +19,7 @@ package v1.controllers.requestParsers.validators
 import support.UnitSpec
 import v1.fixtures.RequestFixtures._
 import v1.models.errors._
-import v1.models.request.{AmendRawData, CreateRawData}
+import v1.models.request.AmendRawData
 
 class AmendValidatorSpec extends UnitSpec {
 
