f6505a146d3d60c7582c2c2b5652eabe951aca69
zachhall1234 <29452017+zachhall1234@users.noreply.github.com>
2020-04-27 15:20:56 +0100

    MTDSA-5254 add validator mock & finished tests

--- test/v1/controllers/requestParsers/AmendRequestModelParserSpec.scala
+++ test/v1/controllers/requestParsers/AmendRequestModelParserSpec.scala
@@ -1,16 +1,36 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package v1.controllers.requestParsers
 
-import junit.framework.Test
 import support.UnitSpec
-import uk.gov.hmrc.domain.Nino
+import v1.mocks.validators.MockAmendValidator
 import v1.models.errors.{BadRequestError, ErrorWrapper, NinoFormatError}
-import v1.models.request.{AmendRawData, AmendRequestData, CreateRawData}
+import v1.models.request.AmendRawData
+import v1.fixtures.AmendRequestFixtures._
 
-class AmendRequestModelParserSpec extends UnitSpec{
+class AmendRequestModelParserSpec extends UnitSpec {
 
   val nino = "AA123456A"
   val invalidNino = "PLKL87654"
   val id = "S4636A77V5KB8625U"
+
+  trait Test extends MockAmendValidator {
+    lazy val parser = new AmendRequestModelParser(mockValidator)
+  }
 
   "parser" should {
     "accept a valid input" when {
@@ -34,38 +54,37 @@ class AmendRequestModelParserSpec extends UnitSpec{
 
         private val result = parser.parseRequest(inputData)
       }
+    }
+    "Reject invalid input" when {
+      "mandatory field is given invalid data" in new Test {
+        val inputData = AmendRawData(nino, id, invalidRequestJson)
 
-      "Reject invalid input" when {
-        "mandatory field is given invalid data" in new Test {
-          val inputData = AmendRawData(nino, id, invalidRequestJson)
+        MockValidator
+          .validate(inputData)
+          .returns(List(BadRequestError))
 
-          MockValidator
-            .validate(inputData)
-            .returns(List(BadRequestError))
+        private val result = parser.parseRequest(inputData)
+        result shouldBe Left(ErrorWrapper(None, List(BadRequestError)))
+      }
+      "Nino format is incorrect" in new Test {
+        val inputData = AmendRawData(invalidNino, id, requestJson)
 
-          private val result = parser.parseRequest(inputData)
-          result shouldBe Left(ErrorWrapper(None,List(BadRequestError)))
-        }
-        "Nino format is incorrect" in new Test {
-          val inputData = AmendRawData(nino, id, requestJson)
+        MockValidator
+          .validate(inputData)
+          .returns(List(NinoFormatError))
 
-          MockValidator
-            .validate(inputData)
-            .returns(List(NinoFormatError))
+        private val result = parser.parseRequest(inputData)
+        result shouldBe Left(ErrorWrapper(None, List(NinoFormatError)))
+      }
+      "Id format is incorrect" in new Test {
+        val inputData = AmendRawData(nino, "id", requestJson)
 
-          private val result = parser.parseRequest(inputData)
-          result shouldBe Left(ErrorWrapper(None,List(NinoFormatError)))
-        }
-        "Id format is incorrect" in new Test {
-          val inputData = AmendRawData(nino, "id", requestJson)
+        MockValidator
+          .validate(inputData)
+          .returns(List(NinoFormatError))
 
-          MockValidator
-            .validate(inputData)
-            .returns(List(NinoFormatError))
-
-          private val result = parser.parseRequest(inputData)
-          result shouldBe Left(ErrorWrapper(None,List(NinoFormatError)))
-        }
+        private val result = parser.parseRequest(inputData)
+        result shouldBe Left(ErrorWrapper(None, List(NinoFormatError)))
       }
     }
   }
--- test/v1/mocks/validators/MockAmendValidator.scala
+++ test/v1/mocks/validators/MockAmendValidator.scala
@@ -1,14 +1,32 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package v1.mocks.validators
 
 import org.scalamock.handlers.CallHandler1
+import org.scalamock.scalatest.MockFactory
+import v1.controllers.requestParsers.validators.AmendValidator
 import v1.models.errors.MtdError
-import v1.models.request.{AmendRawData}
+import v1.models.request.AmendRawData
 
-class MockAmendValidator {
+class MockAmendValidator extends MockFactory{
 
   val mockValidator: AmendValidator = mock[AmendValidator]
 
-  object MockDeleteValidator {
+  object MockValidator {
 
     def validate(data: AmendRawData): CallHandler1[AmendRawData, List[MtdError]] = {
       (mockValidator
