2f5607eaedac1ddcc04e9b1873b999dcbeeba146
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-09-28 13:12:46 +0100

    MTDSA-7157 Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,22 +10,22 @@ object AppDependencies {
 
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.5.0",
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
-    "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
+    "uk.gov.hmrc" %% "domain" % "5.9.0-play-26",
     "uk.gov.hmrc" %% "play-hmrc-api" % "4.1.0-play-26",
-    "org.typelevel" %% "cats-core" % "2.1.0",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.4.4" cross CrossVersion.full),
-    "com.github.ghik" % "silencer-lib" % "1.4.4" % Provided cross CrossVersion.full,
-    "com.chuusai" %% "shapeless" % "2.3.3"
+    "org.typelevel" %% "cats-core" % "2.2.0",
+    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.6.0" cross CrossVersion.full),
+    "com.github.ghik" % "silencer-lib" % "1.6.0" % Provided cross CrossVersion.full,
+    "com.chuusai" %% "shapeless" % "2.4.0-M1"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.1.0"             % scope,
+    "org.scalatest"          %% "scalatest"          % "3.2.0"             % scope,
     "com.vladsch.flexmark"   % "flexmark-all"        % "0.35.10"           % scope,
     "org.scalacheck"         %% "scalacheck"         % "1.14.2"            % scope,
-    "org.scalamock"          %% "scalamock"          % "4.4.0"             % scope,
+    "org.scalamock"          %% "scalamock"          % "5.0.0"             % scope,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"             % scope,
-    "com.github.tomakehurst" % "wiremock"            % "2.25.1"            % scope
+    "com.github.tomakehurst" % "wiremock"            % "2.27.2"            % scope
   )
 
   // Fixes a transitive dependency clash between wiremock and scalatestplus-play
