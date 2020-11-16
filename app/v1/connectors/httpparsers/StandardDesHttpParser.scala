c5f269b8e67e533f5a3f846645e1a5cacd2da386
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2020-11-16 11:35:41 +0000

    update the stub port (#116)
    * update the des port
    
    * Update application.conf

--- app/v1/connectors/httpparsers/StandardDesHttpParser.scala
+++ app/v1/connectors/httpparsers/StandardDesHttpParser.scala
@@ -63,7 +63,7 @@ object StandardDesHttpParser extends HttpParser {
             s"Success response received from DES with correlationId: $correlationId when calling $url")
         successOutcomeFactory(correlationId)
       case BAD_REQUEST | NOT_FOUND | FORBIDDEN | CONFLICT | UNPROCESSABLE_ENTITY => Left(ResponseWrapper(correlationId, parseErrors(response)))
-      case _                                              => Left(ResponseWrapper(correlationId, OutboundError(Seq(DownstreamError))))
+      case _                                                                     => Left(ResponseWrapper(correlationId, OutboundError(Seq(DownstreamError))))
     }
   }
 }
