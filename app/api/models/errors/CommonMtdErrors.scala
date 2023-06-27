957049726f9c3d01910f06d66de13e3f12c60b85
KobithasanVasantharajah <110105165+KobithasanVasantharajah@users.noreply.github.com>
2023-06-27 15:11:55 +0100

    MTDSA-17293: Updated message for RuleTaxYearRangeInvalidError

--- app/api/models/errors/CommonMtdErrors.scala
+++ app/api/models/errors/CommonMtdErrors.scala
@@ -101,7 +101,7 @@ object RuleDateRangeOutOfDate
 object RuleTaxYearRangeInvalidError
     extends MtdError(
       "RULE_TAX_YEAR_RANGE_INVALID",
-      "The specified date range is outside the allowable tax years (the current tax year minus four years)",
+      "The specified tax year is outside the allowable tax years (the current tax year minus four years)",
       BAD_REQUEST)
 
 object RuleTaxYearNotAligned
