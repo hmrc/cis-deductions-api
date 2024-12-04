7de7998c32eb9fb51724d114f1c382dcf0edb874
Daniel Pennington <63240616+DannyPennington97@users.noreply.github.com>
2024-12-04 13:15:46 +0000

    [MTDSA-27865] Rename 'ifs' to 'downstream' in test

--- it/v2/endpoints/DeleteControllerISpec.scala
+++ it/v2/endpoints/DeleteControllerISpec.scala
@@ -100,8 +100,8 @@ class DeleteControllerISpec extends IntegrationBaseSpec {
       }
 
       "downstream service error" when {
-        def serviceErrorTest(ifsStatus: Int, ifsCode: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
-          s"ifs returns an $ifsCode error and status $ifsStatus" in new NonTysTest {
+        def serviceErrorTest(downstreamStatus: Int, downstreamCode: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
+          s"downstream returns an $downstreamCode error and status $downstreamStatus" in new NonTysTest {
             override def setupStubs(): StubMapping = {
               AuditStub.audit()
               AuthStub.authorised()
@@ -109,7 +109,7 @@ class DeleteControllerISpec extends IntegrationBaseSpec {
 
               DownstreamStub
                 .when(method = DownstreamStub.DELETE, uri = downstreamUri)
-                .thenReturn(status = ifsStatus, Json.parse(errorBody(ifsCode)))
+                .thenReturn(status = downstreamStatus, Json.parse(errorBody(downstreamCode)))
             }
 
             val response: WSResponse = await(mtdRequest().delete())
