8604be4217b357b1399f4b2d6369b456e3e57189
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2025-07-07 11:53:26 +0100

    update RULE_INCORRECT_GOV_TEST_SCENARIO message (#233)

--- app/shared/models/errors/CommonMtdErrors.scala
+++ app/shared/models/errors/CommonMtdErrors.scala
@@ -132,4 +132,4 @@ object RuleEndBeforeStartDateError
 object RuleCountryCodeError extends MtdError("RULE_COUNTRY_CODE", "The country code is not a valid ISO 3166-1 alpha-3 country code", BAD_REQUEST)
 
 //Stub Errors
-object RuleIncorrectGovTestScenarioError extends MtdError("RULE_INCORRECT_GOV_TEST_SCENARIO", "The Gov-Test-Scenario was not found", BAD_REQUEST)
+object RuleIncorrectGovTestScenarioError extends MtdError("RULE_INCORRECT_GOV_TEST_SCENARIO", "The supplied Gov-Test-Scenario is not valid", BAD_REQUEST)
