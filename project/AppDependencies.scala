2a808de696f2166aa20cfe2cc8e50056b24962a1
aaocs <132440545+aaocs@users.noreply.github.com>
2026-03-16 09:57:23 +0000

    MTDSA-33358: Update versions in AppDependencies and plugins.sbt

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,17 +23,17 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
-    "com.github.jknack"             % "handlebars"                % "4.3.1"
+    "com.github.jknack"             % "handlebars"                % "4.5.0"
   )
 
   val test: Seq[sbt.ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.scalamock"       %% "scalamock"              % "7.4.0",
-    "org.scalatestplus"   %% "scalacheck-1-18"        % "3.2.19.0"
+    "org.scalamock"       %% "scalamock"              % "7.5.5",
+    "org.scalatestplus"   %% "scalacheck-1-19"        % "3.2.19.0"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq(
-    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.30",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.19.1"
+    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.39",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.1"
   ).map(_ % Test)
 }
