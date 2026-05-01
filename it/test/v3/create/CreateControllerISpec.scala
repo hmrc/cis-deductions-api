8b4b32ae73d74627364abfb4f5d0cf8dd08e4746
Jesse-Karim <198622734+jesse1411@users.noreply.github.com>
2026-05-01 10:12:05 +0100

    MTDSA-34109 Add validation for contractor name in CIS Deductions API

--- it/test/v3/create/CreateControllerISpec.scala
+++ it/test/v3/create/CreateControllerISpec.scala
@@ -22,7 +22,7 @@ import play.api.http.Status.*
 import play.api.libs.json.{JsString, JsValue, Json}
 import play.api.libs.ws.{WSRequest, WSResponse}
 import play.api.test.Helpers.AUTHORIZATION
-import shared.models.errors.{FromDateFormatError, InternalError, MtdError, NinoFormatError, RuleDateRangeInvalidError, RuleIncorrectOrEmptyBodyError, RuleTaxYearNotEndedError, RuleTaxYearNotSupportedError, ToDateFormatError}
+import shared.models.errors.*
 import shared.services.{AuditStub, AuthStub, DownstreamStub, MtdIdLookupStub}
 import shared.support.IntegrationBaseSpec
 import v3.fixtures.CreateRequestFixtures.*
