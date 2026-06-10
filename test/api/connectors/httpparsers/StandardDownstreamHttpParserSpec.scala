910fd2aca877fef7430035f7541050f50bb83566
markstead1 <236363173+markstead1@users.noreply.github.com>
2026-06-10 14:50:44 +0100

    Removed validateJson tests in the StandardDownstreamHttpParserSpec

--- test/api/connectors/httpparsers/StandardDownstreamHttpParserSpec.scala
+++ test/api/connectors/httpparsers/StandardDownstreamHttpParserSpec.scala
@@ -116,48 +116,6 @@ class StandardDownstreamHttpParserSpec extends UnitSpec {
     }
   }
 
-  "validateJson" when {
-    implicit val reads: Reads[SomeDataObject] = Json.reads[SomeDataObject]
-
-    "the JSON is valid" should {
-      "return the parsed model" in {
-        val validJsonResponse: HttpResponse = HttpResponse(
-          OK,
-          expectedJson,
-          Map("CorrelationId" -> Seq(correlationId))
-        )
-
-        val result: Option[SomeDataObject] = validJsonResponse.validateJson[SomeDataObject]
-
-        result shouldBe Some(downstreamResponseData)
-      }
-    }
-
-    "the JSON is invalid" should {
-      "return None" in {
-        val invalidJsonResponse: HttpResponse = HttpResponse(
-          OK,
-          Json.obj("data"     -> 1234),
-          Map("CorrelationId" -> Seq(correlationId))
-        )
-
-        val result: Option[SomeDataObject] = invalidJsonResponse.validateJson[SomeDataObject]
-
-        result shouldBe None
-      }
-    }
-
-    "the response contains no JSON" should {
-      "return None" in {
-        val emptyResponse: HttpResponse = HttpResponse(OK, "", Map("CorrelationId" -> Seq(correlationId)))
-
-        val result: Option[SomeDataObject] = emptyResponse.validateJson[SomeDataObject]
-
-        result shouldBe None
-      }
-    }
-  }
-
   val singleErrorJson: JsValue = Json.parse(
     """
       |{
